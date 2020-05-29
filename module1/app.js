706ab4370bede990fbfe3d46e29d1278c39b39f7
SHRINATH <shrinathn00@gmail.com>
2020-05-29 23:04:22 +0530

    Filters

--- module1/app.js
+++ module1/app.js
@@ -10,7 +10,7 @@
         $scope.displaNumeric=function(){
             var totalNameValue=calculate($scope.name);
             $scope.totalValue=totalNameValue;
-            
+
         };
 
         function calculate(string){
@@ -22,15 +22,37 @@
         }
     });*/
 
-    .controller('DIcontroller', DIcontroller);
+    .controller('DIcontroller', DIcontroller)
+    .filter('replace', ReplaceFilter)
+    .filter('reps', SecondReplace);
+  DIcontroller.$inject = ['$scope', '$filter', 'replaceFilter'];
 
-  function DIcontroller($scope, $filter) {
+  function DIcontroller($scope, $filter, replaceFilter) {
     $scope.name = "Shrinath";
-
+    $scope.cost = 0.4;
+    $scope.simple = 'Your Name';
+    $scope.msg = "hello there how are? are you fine";
     $scope.upper = function() {
-      var upCase = $filter('uppercase');
-      $scope.name = upCase($scope.name);
+      var upCase = $filter('uppercase')($scope.name);
+      $scope.name = upCase;
+      $scope.simple = replaceFilter($scope.simple);
     };
   };
+
+  function ReplaceFilter() {
+    return function(input) {
+      input = input || "";
+      input = input.replace("Name", "Lovely Name");
+      return input;
+    };
+  }
+
+  function SecondReplace() {
+    return function(input, tar, repp) {
+      input = input || "";
+      input = input.replace(tar, repp);
+      return input;
+    };
+  }
 
 })();
